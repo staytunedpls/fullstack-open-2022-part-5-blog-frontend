4566dc14e47c80d5c079240cf7001bd16354c576
Sergey Starikov <sergeystarikov96@gmail.com>
2022-08-30 09:58:48 +0400

    Done 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,29 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisibility] = useState(false);
+
+  const toggleDetailsVisibility = () => {
+    setDetailsVisibility(!detailsVisible);
+  };
+  const blogStyle = { border: "1px solid", margin: "10px", padding: "5px" };
+  const detailsStyle = { display: detailsVisible ? "" : "none" };
+
+  return (
+    <div style={blogStyle}>
+      "{blog.title}" {blog.author}{" "}
+      <button onClick={toggleDetailsVisibility}>
+        {detailsVisible ? "hide" : "view"}
+      </button>
+      <div style={detailsStyle}>
+        url: {blog.url}
+        <br />
+        likes: {blog.likes} <button>like</button>
+        <br />
+        creator: {blog.user.username}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
