12a36a35871c4b297b2059c621ef54bda3c8427c
Sergey Starikov <sergeystarikov96@gmail.com>
2022-09-09 23:42:09 +0400

    Done 5.14 (click requires await)

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Blog from './Blog';
+import userEvent from '@testing-library/user-event';
 
 test('blog renders title and author, but not url or number of likes by default', () => {
   const blog = {
@@ -26,4 +27,32 @@ test('blog renders title and author, but not url or number of likes by default',
 
   const detailsThroughLikes = screen.getByText('1000', { exact: false });
   expect(detailsThroughLikes).not.toBeVisible();
+});
+
+test('url and likes shown when the "show" button is clicked', async () => {
+  const blog = {
+    title: 'test blog title',
+    author: 'test blog author',
+    url: 'test blog url',
+    likes: 1000,
+    user: { id: 'userid', username: 'user' },
+    id: 'blogid',
+  };
+  const loggedUser = { id: 'userid' };
+
+  const { container } = render(
+    <Blog blog={blog} removeBlog={() => {}} loggedUser={loggedUser} />
+  );
+
+  const showButton = screen.getByText('view');
+  expect(showButton).not.toBeNull();
+  await userEvent.click(showButton);
+  const details = container.querySelector('.blog-details');
+  expect(details).toBeVisible();
+
+  const detailsThroughUrl = screen.getByText('test blog url', { exact: false });
+  expect(detailsThroughUrl).toBeVisible();
+
+  const detailsThroughLikes = screen.getByText('1000', { exact: false });
+  expect(detailsThroughLikes).toBeVisible();
 });
