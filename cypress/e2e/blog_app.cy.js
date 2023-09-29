448e60578e6b404bd9d5e4c5302517f017ed10dd
Sergey Starikov <sergeystarikov96@gmail.com>
2023-09-30 00:33:03 +0100

    Done 5.18 + bonus

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -14,10 +14,16 @@ describe('blog app', () => {
     cy.contains('Log in to application');
     cy.contains('username');
   });
-  it('try to login', () => {
+  it('correct login works', () => {
     cy.get('#username').type('Test username 1');
     cy.get('#password').type('Test password 1');
     cy.get('#login-btn').click();
     cy.contains('logged in');
+  });
+  it('incorrect login fails miserably', () => {
+    cy.get('#username').type('Test username 1');
+    cy.get('#password').type('Test password 2');
+    cy.get('#login-btn').click();
+    cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)');
   });
 });
